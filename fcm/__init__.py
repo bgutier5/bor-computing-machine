7c43e85d707f4dac8182517bebcefb406a5adba4
Boris Daniel Gutierrez Cortes <bdgc1987@gmail.com>
2017-07-25 11:29:48 -0700

    my module

---
+++ fcm/__init__.py
@@ -0,0 +1,8 @@
+"""
+
+This is the base file of the friendly computing mathine
+
+"""
+
+from . import math
+
---
+++ fcm/math.py
@@ -0,0 +1,19 @@
+"""
+
+A small set of functions for doing math operations
+
+"""
+
+# wire a function named add that adds tow values
+
+def add(number1 , number2):
+    """
+
+    Function that adds two arguments
+
+    """
+    sumation = number1 + number2
+    return sumation
+
+
+
